d21c80ce36172332746a36c7e8ee2df9dbec982c
Ahmad <diracdeltain0@gmail.com>
2026-08-08 21:09:30 +0330

    Update TermuxSetup.sh

--- TermuxSetup.sh
+++ TermuxSetup.sh
@@ -14,7 +14,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-chmod +x ip-bypass-plus-frag/ip-bypass-plus-frag
+chmod +x ip-bypass-plus-frag/termux/ip-bypass-plus-frag
 
 if [ -n "$ZSH_VERSION" ]; then
     SHELL_RC="$HOME/.zshrc"
@@ -22,8 +22,8 @@ else
     SHELL_RC="$HOME/.bashrc"
 fi
 
-if ! grep -q "alias i='~/ip-bypass-plus-frag/ip-bypass-plus-frag'" "$SHELL_RC" 2>/dev/null; then
-    echo "alias i='~/ip-bypass-plus-frag/ip-bypass-plus-frag'" >> "$SHELL_RC"
+if ! grep -q "alias i='~/ip-bypass-plus-frag/termux/ip-bypass-plus-frag'" "$SHELL_RC" 2>/dev/null; then
+    echo "alias i='~/ip-bypass-plus-frag/termux/ip-bypass-plus-frag'" >> "$SHELL_RC"
     echo "Alias added to $SHELL_RC"
 else
     echo "Alias already exists in $SHELL_RC"
